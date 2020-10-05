1d116e35ac4c92f05128ba0487a00129473053e2
mariamfahes <fahesmariam20@gmail.com>
2020-10-05 16:23:05 +0300

    step4

---
+++ step-4.php
@@ -0,0 +1,6 @@
+<?php
+$name="mariam";
+$position="tech consultant";
+$github_url ="//github.com/gk-git";
+echo "Hello, I'm ". $name . "I'm a". $position . " please check my github link https:" . $github_url;
+?>
